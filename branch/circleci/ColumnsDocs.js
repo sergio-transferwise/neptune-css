6bd3df712e98c09e735aa2d04085442199810c24
Carlos Matallín <carlos.matallin@transferwise.com>
2019-05-01 14:50:30 +0100

    Clean

--- branch/circleci/ColumnsDocs.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Columns = () => (
-  <section id="columns" className="wrap--content section">
-    <h1 className="colored-dot">Column system</h1>
-  </section>
-    );
-
-export default Columns;
--- docs/TableDocs.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Table = () => (
-  <section id="table" className="wrap--page section">
-    <h1>Table</h1>
-  </section>
-    );
-
-export default Table;
